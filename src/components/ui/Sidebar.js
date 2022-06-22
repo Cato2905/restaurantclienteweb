246a37267872c7eb5aa0ad7e7d4f9f172120eabe
Matias Riquelme <matyas1996mayo@gmail.com>
2022-06-22 05:39:11 -0400

    277

--- src/components/ui/Sidebar.js
+++ src/components/ui/Sidebar.js
@@ -1,11 +1,19 @@
 import React from 'react';
-
+import { NavLink } from 'react-router-dom';
+import Ordenes from './../paginas/Ordenes';
 
 const Sidebar = () => {
     return (  
         <div className='md:w-1/4 xl:w:1/5 bg-gray-800'>
             <div className='p-6'>
                 <p className='uppercase text-white text-2xl tracking-wide text-center font-bold'>Okasaki</p>
+
+                <p className='mt-3 text-gray-500'> Administra el menu en las siguientes opciones: </p>
+
+                <nav className='mt-6'>
+                    <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" activeClassName="text-yellow-500" exact="true" to="/">Ordenes</NavLink>
+                    <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" activeClassName="text-yellow-500" exact="true" to="/menu">Menú</NavLink>
+                </nav>
             </div>
         </div>
     );
